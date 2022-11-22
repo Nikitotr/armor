425322b0558b3d7b3302a708c7833bd053d3aa55
Nikitotr <79308670+Nikitotr@users.noreply.github.com>
2022-11-22 15:06:20 +0200

    Add files via upload

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ6hPbPsNZzg_XeWsH6Ok_6hksiIMyyIQ",
+  authDomain: "auth-weapon.firebaseapp.com",
+  projectId: "auth-weapon",
+  storageBucket: "auth-weapon.appspot.com",
+  messagingSenderId: "956404225678",
+  appId: "1:956404225678:web:207a44d2f3b7bea5c11bdf"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
